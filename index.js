908597ae614eac5f994559c1a2c7617956b35ab9
Douglas Obara <douglasobara97@gmail.com>
2022-06-13 08:31:28 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+/*
+    after index.js is processed
+    no longer has DOM node 'main#main'
+    has a 'newHeader' variable that points to node 'h1#victory'
+    has a 'newHeader' variable that points to node 'h1#victory'
+    has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+ */
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Douglas obara is the champion";
